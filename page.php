e4e4313c5ff079c4d794a425ed257f51d11a3ae2
AZYxV <nathan.sichouc@le-campus-numerique.fr>
2023-01-03 13:26:34 +0100

    exo php

---
+++ page.php
@@ -0,0 +1,34 @@
+    <?php include 'header.php' ?>
+    <main>
+        <div id="carouselExampleIndicators" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="assets/img1.png" class="d-block w-100" alt="Illustation">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img1.png" class="d-block w-100" alt="Illustration">
+                </div>
+                <div class="carousel-item">
+                    <img src="assets/img1.png" class="d-block w-100" alt="Illustration">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Précédent</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Suivant</span>
+            </button>
+        </div>
+        <h1 class="text-center p-4">Lorem Ipsum</h1>
+        <p class="text-center m-5"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut purus aliquet, pulvinar tortor vel, rutrum lacus. Sed pulvinar eros at justo malesuada, vitae tempor massa malesuada. Maecenas dui leo, pellentesque nec tempus vel, pharetra sed ipsum. Etiam tempor diam a lorem vestibulum tempor. Sed vel accumsan tortor. Donec eu tempus dui. Sed lorem est, tempor id lacinia et, efficitur quis dui. Aliquam dapibus posuere neque ac scelerisque.
+
+            In mattis magna ligula, nec tempus eros maximus a. Integer egestas quam sed turpis lacinia commodo. Proin nec diam ut ligula viverra aliquam. Duis tincidunt semper sem vitae eleifend. Curabitur pretium dolor eu ipsum tempor, mattis fermentum lacus imperdiet. Nulla eu dapibus arcu, et volutpat leo. Donec ornare ornare quam eget facilisis. Suspendisse sodales nibh eget turpis hendrerit, ut malesuada elit ornare. Fusce a commodo dolor, eget scelerisque ligula. Donec malesuada aliquet sem, vitae luctus est aliquam sit amet. Vivamus convallis volutpat lorem in maximus. Vivamus porttitor convallis consectetur. </p>
+    </main>
+    <?php include 'footer.php' ?>
