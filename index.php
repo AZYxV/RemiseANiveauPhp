9475763722632f249bab054ad0cca798d48927ef
AZYxV <nathan.sichouc@le-campus-numerique.fr>
2023-01-03 14:13:53 +0100

    exo 3

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-
+if($_GET['page'] == 'page'){
+    include 'page.php';
+} elseif ($_GET['page'] == 'header') {
+    include 'header.php';
+} elseif ($_GET['page'] == 'footer') {
+    include 'footer.php';
+} else {
+    include 'notFound.php';
+}
