8287adc5e9bf28ea9a81412490a711d29eb48741
AZYxV <nathan.sichouc@le-campus-numerique.fr>
2023-01-04 15:26:26 +0100

    exo 4.4

--- pages/contact.php
+++ pages/contact.php
@@ -1,21 +1,38 @@
-<?php include 'header.php' ?>
 <?php
+
+include 'header.php';
+
 if(isset($_POST['submit'])) {
+
+    $valid = true;
+
+    $civilite = filter_input(INPUT_POST, 'civilite', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $nom = filter_input(INPUT_POST, 'nom', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $prenom = filter_input(INPUT_POST, 'prenom', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $raison = filter_input(INPUT_POST, 'raison', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
     foreach ($_POST as $key => $value) {
         if (empty($value)) {
             ${"error$key"} = 'Veuillez completer le champs '.$key;
+            $valid = false;
         }
     }
 
-    $civilite = filter_input(INPUT_POST, 'civilite', FILTER_DEFAULT);
-    $nom = filter_input(INPUT_POST, 'nom', FILTER_DEFAULT);
-    $prenom = filter_input(INPUT_POST, 'prenom', FILTER_DEFAULT);
-    $email = filter_input(INPUT_POST, 'email', FILTER_DEFAULT);
-    $raison = filter_input(INPUT_POST, 'raison', FILTER_DEFAULT);
-    $message = filter_input(INPUT_POST, 'message', FILTER_DEFAULT);
 
-    #file_put_contents('Contact'.date("Y-m-d H:i:s"), $civilite." | ".$nom." | ".$prenom." | ".$email." | ".$raison." | ".$message);
+    if(strlen($_POST['message'])<5) {
+        echo "Votre message dois contenir 5 caractères min";
+        $valid = false;
+    }
+    if (!$email) {
+        echo "Adresse email invalide";
+        $valid = false;
+    }
+    if($valid) {
+        file_put_contents('Contact'.date("Y-m-d H:i:s"), $civilite." | ".$nom." | ".$prenom." | ".$email." | ".$raison." | ".$message);
+    }
+
 }
 var_dump($_POST)
 ?>
@@ -23,7 +40,7 @@ var_dump($_POST)
     <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
         <label for="civilite">Civilité :</label>
         <select name="civilite" id="civilite">
-            <option value="M.">M.</option>
+            <option value="M">M</option>
             <option value="Mme">Mme</option>
         </select>
         <br>
@@ -50,11 +67,11 @@ var_dump($_POST)
         } ?>
         <label for="raison">Raison du contact :</label>
         <br>
-        <input type="radio" name="raison" id="raison1" value="proposition d'emploi">Proposition d'emploi
+        <input type="radio" name="raison" value="proposition d'emploi">Proposition d'emploi
         <br>
-        <input type="radio" name="raison" id="raison2" value="demande d'information">Demande d'information
+        <input type="radio" name="raison" value="demande d'information">Demande d'information
         <br>
-        <input type="radio" name="raison" id="raison3" value="prestations">Prestations
+        <input type="radio" name="raison" value="prestations">Prestations
         <br>
         <?php if($errormessage) {
             echo $errormessage;
